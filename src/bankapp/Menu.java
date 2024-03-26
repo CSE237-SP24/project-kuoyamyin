1d098e7ffd26690f3c1e042cfe4f281a65cfa161
ScottYama <s.c.yamamoto@wustl.edu>
2024-03-26 15:56:56 -0500

    finished objects and getBalance

--- src/bankapp/Menu.java
+++ src/bankapp/Menu.java
@@ -3,45 +3,50 @@ package bankapp;
 import java.util.Scanner;
 
 public class Menu {
-
+	
 	private Scanner in;
 	private BankAccount account;
-	
-	//not tested
+
 	public static void main(String[] args) {
 		Menu mainMenu = new Menu();
 		mainMenu.displayingOptions();
-		double amount = mainMenu.getValidUserInput();
-		mainMenu.processingUserSelection(amount);
+		int choice = mainMenu.getValidUserInput();
+		mainMenu.processingUserSelection(choice);
 	}
 	
-	//Constructor
 	public Menu() {
 		this.in = new Scanner(System.in);
 		this.account = new BankAccount();
 	}
 	
-	//Code that just displays stuff - no tests needed
 	public void displayingOptions() {
-		System.out.println("How much money do you want to deposit?");
+		System.out.println("1. Check balance");
+		System.out.println("2. Deposit money");
+		System.out.println("3. Withdraw money");
+		System.out.println("Select an action (enter number):");
 	}
 	
-	//Code that gets user input
-	//No tests needed...for now (probably discuss in future class)
-	public double getValidUserInput() {
-		double amount = in.nextDouble();
-		while(amount < 0) {
-			System.out.println("Invalid value!");
-			System.out.println("How much money do you want to deposit?");
-			amount = in.nextDouble();
+	public int getValidUserInput() {
+		int choice = in.nextInt();
+		while(choice < 1 || choice > 3) {
+			System.out.println("Invalid choice.");
+			System.out.println("1. Check balance");
+			System.out.println("2. Deposit money");
+			System.out.println("3. Withdraw money");
+			System.out.println("Select an action (enter number):");
+			choice = in.nextInt();
 		}
-		return amount;
+		return choice;
 	}
 	
-	//Does work - needs tests
-	public void processingUserSelection(double amount) {
-		account.deposit(amount);
-		System.out.println("Your balance is now: " + account.getBalance());
+	public void processingUserSelection(int choice) {
+		if (choice == 1) {
+			System.out.println("Balance: " + account.getBalance());
+		} else if (choice == 2) {
+			// call methods for running deposit
+		} else if (choice == 3) {
+			// call methods for running withdraw
+		}
 	}
 	
 	public BankAccount getAccount() {
