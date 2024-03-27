f43e4843ffd70fa0280318319559d7a55a2d9dde
ScottYama <s.c.yamamoto@wustl.edu>
2024-03-27 15:11:50 -0500

    user can not do multiple actions in a row

--- src/bankapp/Menu.java
+++ src/bankapp/Menu.java
@@ -6,33 +6,44 @@ public class Menu {
 	
 	private Scanner in;
 	private BankAccount account;
+	private boolean exit;
 
 	public static void main(String[] args) {
 		Menu mainMenu = new Menu();
-		mainMenu.displayingOptions();
-		int choice = mainMenu.getValidUserInput();
-		mainMenu.processingUserSelection(choice);
+		mainMenu.runBankFeatures();
 	}
 	
 	public Menu() {
 		this.in = new Scanner(System.in);
 		this.account = new BankAccount();
+		this.exit = false;
+	}
+	
+	public void runBankFeatures() {
+		while(!exit) {
+			this.displayingOptions();
+			int choice = this.getValidUserInput();
+			this.processingUserSelection(choice);
+		}
 	}
 	
 	public void displayingOptions() {
+		System.out.println("");
 		System.out.println("1. Check balance");
 		System.out.println("2. Deposit money");
 		System.out.println("3. Withdraw money");
+		System.out.println("4. Exit");
 		System.out.println("Select an action (enter number):");
 	}
 	
 	public int getValidUserInput() {
 		int choice = in.nextInt();
-		while(choice < 1 || choice > 3) {
+		while(choice < 1 || choice > 4) {
 			System.out.println("Invalid choice.");
 			System.out.println("1. Check balance");
 			System.out.println("2. Deposit money");
 			System.out.println("3. Withdraw money");
+			System.out.println("4. Exit");
 			System.out.println("Select an action (enter number):");
 			choice = in.nextInt();
 		}
@@ -59,6 +70,9 @@ public class Menu {
 			double withdrawAmount = getValidWithdrawAmount(); 
 			account.withdraw(withdrawAmount); // when can i call getValidAmount?
 			System.out.println("Amount withdrew: " + withdrawAmount + ". New balance = " + account.getBalance());
+		} else if (choice == 4) {
+			exit = true;
+			System.out.println("Account exited");
 		}
 	}
 	
